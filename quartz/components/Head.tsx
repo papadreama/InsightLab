2ef9d6fd643bd5ebf8501b28b4e39a269dced642
papadreama <papadopoulos.dimitri@gmail.com>
2024-09-02 19:41:41 -0400

    Quartz sync: Sep 2, 2024, 7:41 PM

--- quartz/components/Head.tsx
+++ quartz/components/Head.tsx
@@ -27,6 +27,7 @@ export default (() => {
             <link rel="preconnect" href="https://fonts.googleapis.com" />
             <link rel="preconnect" href="https://fonts.gstatic.com" />
             <link rel="stylesheet" href={googleFontHref(cfg.theme)} />
+            <link href="https://fonts.googleapis.com/css2?family=Abril+Fatface&display=swap" rel="stylesheet"></link>
           </>
         )}
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
